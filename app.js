5f46c85b0ee0ad29739b4138f7de38ea2afc9764
HangorG <gaba93@yandex.ru>
2020-11-02 19:20:38 +0300

    added chart module

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import ItemCtrl from "./modules/itemctrl.js";
 import StorageCtrl from "./modules/storagectrl.js";
 import UICtrl from "./modules/uictrl.js";
+import ChartCtrl from "./modules/chartctrl.js"
 
 // Main module
 
@@ -223,25 +224,6 @@ const clearAllItemsClick = function (e) {
   e.preventDefault();
 };
 
-let ctx = document.getElementById('myChart').getContext('2d');
-let chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'bar',
-
-    // The data for our dataset
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-            label: 'Calories',
-            backgroundColor: 'rgb(0, 99, 132)',
-            borderColor: 'rgb(0, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45]
-        }]
-    },
-
-    // Configuration options go here
-    options: {}
-});
 
 
 
---
+++ modules/chartctrl.js
@@ -0,0 +1,26 @@
+
+
+let ctx = document.getElementById('myChart').getContext('2d');
+let chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'bar',
+
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'Calories',
+            backgroundColor: 'rgb(0, 99, 132)',
+            borderColor: 'rgb(0, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+
+export default {
+  ctx,
+  chart
+}
